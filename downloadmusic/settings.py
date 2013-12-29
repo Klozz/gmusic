ac050308ece5c68eee362f23d307f4c1ceeb5b93
Irving Rodriguez <irving.prog@gmail.com>
2013-12-28 23:39:23 -0600

    Update

--- downloadmusic/settings.py
+++ downloadmusic/settings.py
@@ -52,12 +52,12 @@ WSGI_APPLICATION = 'downloadmusic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}"""
+}
 
 DATABASES['default'] =  dj_database_url.config()
 

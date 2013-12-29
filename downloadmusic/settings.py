a04688dc1f58817876c973acb9926e5e15da843f
Irving Rodriguez <irving.prog@gmail.com>
2013-12-28 23:16:11 -0600

    Setings update

--- downloadmusic/settings.py
+++ downloadmusic/settings.py
@@ -58,10 +58,11 @@ WSGI_APPLICATION = 'downloadmusic.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+      'default' : {
+         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+         'NAME' : 'db_name',
+         #setear USERNAME Y PASS si necesitan.
+      }
 }
 
 # Internationalization

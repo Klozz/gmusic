635620a9f84620413d7e63b26a848fb2360aafa0
Irving Rodriguez <irving.prog@gmail.com>
2014-01-06 12:20:54 -0600

    Refactorizando código y utilizando modulo requests

--- downloader/views.py
+++ downloader/views.py
@@ -1,51 +1,41 @@
 # -*- coding: utf-8 -*-
 from django.template import RequestContext
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from downloader.models import *
-from httplib2 import Http
-from django.contrib import messages
+from django.core import serializers
 import re
 import json
-from django.core import serializers
-
-
-http = Http()
+import requests
+import time
 
 def home(request):
-	recent_searchs = Search.objects.all().order_by('-date')[:5]
-	return render_to_response('home.html', locals(), RequestContext(request))
+        recent_searchs = Search.objects.all().order_by('-date')[:5]
+        return render_to_response('home.html', locals(), RequestContext(request))
 
-def ultimas_cancones_json(request):
-	recent_searchs = Search.objects.all().order_by('-date')[:5]
-	searchs = serializers.serialize('json', recent_searchs, sort_keys=True, indent=4)
-	return HttpResponse(searchs, content_type="application/json",mimetype='application/json')
+def ultimas_canciones_json(request):
+        recent_searchs = Search.objects.all().order_by('-date')[:5]
+        searchs = serializers.serialize('json', recent_searchs, sort_keys=True, indent=4)
+        return HttpResponse(searchs, content_type="application/json",mimetype='application/json')
 
 def buscar_cancion_json(request):
-	search = request.GET.get('name_song')
-	contenido1 = http.request("http://www.goear.com/search/"+search)[1]
-	contenido2 = http.request("http://www.goear.com/search/"+search)[1]
-	contenido = contenido1 + contenido2
-	songs = []
-	names = re.findall(r'<span class="song">([-.\s\w]+)</span>',contenido)
-	artist = re.findall(r'<span class="group">([-.\s\w]+)</span>', contenido)
-	ids = re.findall(r'href="http://www.goear.com/listen/([\w\d]+)/',contenido)
-	qualitys = re.findall(r'<li class="kbps radius_3">([\d]+)<abbr title="Kilobit por segundo">kbps</abbr></li>', contenido)
-	lengs = re.findall(r'<li class="length radius_3">([-:\d]+)</li>', contenido)
+        search = request.GET.get('name_song')
+        contenido1 = requests.get("http://www.goear.com/search/"+search)
+        contenido2 = requests.get("http://www.goear.com/search/"+search+"/1")
+        contenido = contenido1.text + contenido2.text
 
-	if len(names) == 0:
-		songs = None
-		result = False
-	else:
-		for i in range(0,len(names)):
-			try: 
-				songs.append({'name':names[i], 'artist':artist[i],'id':ids[i],'quality': qualitys[i], 'len': lengs[i]})
-			except:
-				pass
+        names = re.findall(r'<span class="song">([-.\s\w]+)</span>',contenido)
+        artists = re.findall(r'<span class="group">([-.\s\w]+)</span>', contenido)
+        ids = re.findall(r'href="http://www.goear.com/listen/([\w\d]+)/',contenido)
+        qualitys = re.findall(r'<li class="kbps radius_3">([\d]+)<abbr title="Kilobit por segundo">kbps</abbr></li>', contenido)
+        lengs = re.findall(r'<li class="length radius_3">([-:\d]+)</li>', contenido)
+     
+        songs = (None if not len(names) else [])
 
-		result = True
+        for i, name in enumerate(names):
+                songs.append({'name': name, 'artist': artists[i], 'id': ids[i], 'quality': qualitys[i], 'len': lengs[i]})
 
-	model_search = Search(name=search.replace('-',' '),result=result)
-	model_search.save()
+        model_search = Search(name=search.replace('-',' '),result=(False if not songs else True))
+        model_search.save()
 
-	return HttpResponse(json.dumps(songs, sort_keys=True, indent=4), content_type="application/json",mimetype='application/json')
+        return HttpResponse(json.dumps(songs, sort_keys=True, indent=4), content_type="application/json",mimetype='application/json')

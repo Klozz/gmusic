221d8b83b08ddcf20bf23f54fcb0dc9aa74b4c10
Irving Rodriguez <irving.prog@gmail.com>
2014-01-06 12:29:07 -0600

    urls

--- downloadmusic/urls.py
+++ downloadmusic/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     url(r'^json/search/$', buscar_cancion_json, name='buscar_cancion_json'),
-    url(r'^json/recent_searchs/$', ultimas_cancones_json, name='ultimas_cancones_json'),
+    url(r'^json/recent_searchs/$', ultimas_canciones_json, name='ultimas_canciones_json'),
     url(r'^admin/', include(admin.site.urls)),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
